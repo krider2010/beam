bdb40a1b1eebd53c18ce2021fcb9f668d3ba642d
Claire <claire.knight@krider.co.uk>
2022-02-25 16:59:40 +0000

    Support Netlify

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -62,6 +62,10 @@ function getBaseUrl() {
     return `https://${process.env.VERCEL_URL}`
   }
 
+  if (process.env.NETLIFY && process.env.URL) {
+    return `https://${process.env.URL}`
+  }
+
   return `http://localhost:${process.env.PORT ?? 3000}`
 }
 
